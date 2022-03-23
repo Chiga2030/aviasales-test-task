82f67e880286b6238060cf6983532352d936b7ca
Anton <chiga2030@gmail.com>
2022-03-23 23:27:29 +0300

    added test for Text component

---
+++ src/components/common/Text/Text.test.js
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react';
+import Text, { TextVariant, TextNotation } from './';
+
+describe('В компоненте Text', () => {
+  test('без пропа varian, компонент не отрисуется', () => {
+    const {
+      container,
+    } = render(<Text>sample text</Text>);
+
+    expect(container.textContent).toBe('');
+  });
+
+
+  test('текст отрисовывается если выбран variant', () => {
+    const {
+      getByText,
+    } = render(<Text variant={ TextVariant.HEADING_2 }>sample text</Text>);
+
+    expect(getByText('sample text')).toBeInTheDocument();
+  });
+
+
+  test('навешен класс uppercase, при prop.notation == uppercase', () => {
+    const {
+      container,
+    } = render(
+      <Text
+        variant={ TextVariant.HEADING_2 }
+        notation={ TextNotation.UPPERCASE }
+      >sample text</Text>);
+
+    expect(container.querySelector('.uppercase')).toBeInTheDocument();
+  });
+});
+
+
+describe('Для пропа variant', () => {
+  test('heading 2 уровня должен быть отрисован <h2/>', () => {
+    const {
+      container,
+    } = render(<Text variant={ TextVariant.HEADING_2 }>sample text</Text>);
+
+    expect(container.querySelector('h2')).toBeInTheDocument();
+  });
+});
