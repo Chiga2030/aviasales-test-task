12e9e81aa94b2c6e8e46d4865e25369a4b2db03a
Anton <chiga2030@gmail.com>
2022-03-23 18:36:43 +0300

    update test for app component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,6 +8,18 @@ import App from './App';
 describe('В компоненте App', () => {
   test('присутствует header', () => {
     const { container } = render(<App />);
-    expect(container.querySelector('header')).toBeInTheDocument()
+    expect(container.querySelector('header')).toBeInTheDocument();
+  });
+
+
+  test('присутствует sidebar', () => {
+    const { container } = render(<App />);
+    expect(container.querySelector('aside')).toBeInTheDocument();
+  });
+
+
+  test('присутствует main', () => {
+    const { container } = render(<App />);
+    expect(container.querySelector('main')).toBeInTheDocument();
   });
 });
